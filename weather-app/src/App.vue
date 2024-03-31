e3d9ad65cf1909b0a202023841e94bd868b43d59
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2024-03-31 22:04:29 +0900

    init project

---
+++ weather-app/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import SearchField from './components/SearchField.vue'
+</script>
+
+<template>
+  <main>
+    <div class="text-center mb-6">
+      {{
+        new Date()
+          .toLocaleDateString('ja-JP', { year: 'numeric', month: '2-digit', day: '2-digit' })
+          .replace(/\//g, '/')
+      }}
+      （{{ ['日', '月', '火', '水', '木', '金', '土'][new Date().getDay()] }}）
+    </div>
+    <div>
+      <SearchField />
+    </div>
+  </main>
+</template>
