cccbbe431a7463696649d40517845c462e9d411e
Hayato Kamiyama <60092195+kami8ma8810@users.noreply.github.com>
2024-04-01 09:32:16 +0900

    add SearchField

--- weather-app/src/components/SearchField.vue
+++ weather-app/src/components/SearchField.vue
@@ -1,10 +1,32 @@
 <script setup>
-import { reactive } from 'vue';
-
+import { reactive } from 'vue'
 
 const searchTerm = reactive({
   query: '',
-});
+  timeout: null,
+  results: null
+})
+
+const handleSearch = () => {
+  clearTimeout(searchTerm.timeout)
+  searchTerm.timeout = setTimeout(async () => {
+    if (searchTerm.querry) return
+
+    const res = await fetch(
+      `http://api.weatherapi.com/v1/search.json?key=079ef26b3b8846e3849131124243103&q=${searchTerm.query}`
+    )
+    const data = await res.json()
+    searchTerm.results = data
+    console.log(searchTerm.results)
+  }, 500)
+}
+const getWeather = async (id) => {
+  const res = await fetch(
+    `http://api.weatherapi.com/v1/forecast.json?key=079ef26b3b8846e3849131124243103&q=id:${id}&days=3&aqi=no&alerts=no`
+  )
+  const data = await res.json()
+  console.log(data)
+}
 </script>
 
 <template>
@@ -18,13 +40,21 @@ const searchTerm = reactive({
           placeholder="Search for a place"
           class="rounded-r-lg p-2 border-0 outline-0 focus:ring-2 focus:ring-indigo-600 ring-inset w-full"
           v-model="searchTerm.query"
+          @input="handleSearch"
         />
       </div>
     </form>
     <!-- search suggestions -->
     <div class="bg-white my-2 rounded-lg shadow-lg">
-      <div>
-        <button class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"></button>
+      <div v-if="searchTerm.results">
+        <div v-for="place in searchTerm.results" :key="place.id">
+          <button
+            @click="getWeather(place.id)"
+            class="px-3 my-2 hover:text-indigo-600 hover:font-bold w-full text-left"
+          >
+            {{ place.name }},{{ place.region }},{{ place.country }}
+          </button>
+        </div>
       </div>
     </div>
   </div>
